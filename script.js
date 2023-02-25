fb4f0fc15b99d04976703334456dcf16ec0531da
Devin McDaniel <dmcdaniel9@gmail.com>
2023-02-25 23:42:45 +0000

    bug fix
    Fixed issue with incomplete hex codes being produced

--- script.js
+++ script.js
@@ -15,6 +15,17 @@ function getNewColor() {
   let firstPart = hexCodeParts[randomNumber()].toString() + hexCodeParts[randomNumber()].toString();
   let secondPart = hexCodeParts[randomNumber()].toString() + hexCodeParts[randomNumber()].toString();
   let thirdPart = hexCodeParts[randomNumber()].toString() + hexCodeParts[randomNumber()].toString();
+  
+  function checkValue(value) {
+    if (value.length < 2) {
+      value += value;
+      return value;
+    } else return value;
+  }
+
+  firstPart = checkValue(firstPart);
+  secondPart = checkValue(secondPart);
+  thirdPart = checkValue(thirdPart);
 
   let hexCode = firstPart + secondPart + thirdPart
 
