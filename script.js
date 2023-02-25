97cf88e63aea7ea52d69743356ae0364840082f8
Devin McDaniel <dmcdaniel9@gmail.com>
2023-02-25 22:46:41 +0000

    bug fix
    Resolved undefined value by editing random number function and ensuring
    type coercions

--- script.js
+++ script.js
@@ -5,16 +5,16 @@ const hexCodeElement = document.querySelector(".color-code");
 const changeButton = document.querySelector(".change-color");
 const hexCodeParts = ["a", "b", "c", "d", "e", "f", "0", "1", "2", "3", "4", "5", "6", "7", "8", "9"];
 
-let randomNumber = () => Math.floor(Math.random() * hexCodeParts.length + 1);
+let randomNumber = () => Math.floor(Math.random() * hexCodeParts.length);
 
 
 changeButton.addEventListener('click', getNewColor);
 
 function getNewColor() {
 
-  let firstPart = hexCodeParts[randomNumber()] + hexCodeParts[randomNumber()];
-  let secondPart = hexCodeParts[randomNumber()] + hexCodeParts[randomNumber()];
-  let thirdPart = hexCodeParts[randomNumber()] + hexCodeParts[randomNumber()];
+  let firstPart = hexCodeParts[randomNumber()].toString() + hexCodeParts[randomNumber()].toString();
+  let secondPart = hexCodeParts[randomNumber()].toString() + hexCodeParts[randomNumber()].toString();
+  let thirdPart = hexCodeParts[randomNumber()].toString() + hexCodeParts[randomNumber()].toString();
 
   let hexCode = firstPart + secondPart + thirdPart
 
